930ffe77cb9cca49e86501fa9f186963fbd66493
arashi87 <arasi27676271@gmail.com>
2023-03-07 17:02:54 +0800

    feat(repositories): init repositories use to be CRUD base class

---
+++ api/repositories/base.py
@@ -0,0 +1,28 @@
+from typing import Generic, Type, TypeVar
+
+from models.base import Base
+from pydantic import BaseModel
+from sqlalchemy.ext.asyncio import AsyncSession
+
+Model = TypeVar("Model", bound=Base)
+CreateModel = TypeVar("CreateModel", bound=BaseModel)
+UpdateModel = TypeVar("UpdateModel", bound=BaseModel)
+
+"""
+Base CRUD class for all models
+"""
+
+
+class BaseRepository(Generic[Model]):
+    def __init__(self, model: Type[Model]):
+        self.model = model
+
+    async def create(self, db: AsyncSession, obj_in: CreateModel) -> Model:
+        db_obj = self.model(**obj_in.dict())
+        db.add(db_obj)
+        await db.commit()
+        await db.refresh(db_obj)
+        return db_obj
+
+    async def get(self, db: AsyncSession, id: int):
+        return await db.get(self.model, id)
