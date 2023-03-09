4af817c42ca723b820a819567943d0e0feb25783
arashi87 <arasi27676271@gmail.com>
2023-03-10 00:49:06 +0800

    test(user): add `user:get_user` endpoint testing

--- api/tests/test_user.py
+++ api/tests/test_user.py
@@ -35,3 +35,31 @@ async def test_create_user_exists(app: FastAPI):
         resp = await ac.post(url=url, json=data)
         assert resp.status_code == 400
         assert resp.json() == result
+
+
+""" Test get user endpoint
+@router get /user/
+@status_code 200
+@response_model schemas.UserWithoutPassword
+@name user:get_user
+"""
+
+
+async def test_get_user_success(app: FastAPI):
+    url = app.url_path_for(name="user:get_user", user_id=1)
+    result = {"id": 1, "name": "default", "email": "default@gmail.com"}
+
+    async with AsyncClient(app=APP, base_url="https://localhost") as ac:
+        resp = await ac.get(url=url)
+        assert resp.status_code == 200
+        assert resp.json() == result
+
+
+async def test_get_user_none_exists(app: FastAPI):
+    url = app.url_path_for(name="user:get_user", user_id=100)
+    result = {"detail": "User not found"}
+
+    async with AsyncClient(app=APP, base_url="https://localhost") as ac:
+        resp = await ac.get(url=url)
+        assert resp.status_code == 404
+        assert resp.json() == result
